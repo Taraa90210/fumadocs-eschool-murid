7caec0b39031a6f0a0c1530913f5e989c456b98e
Taraa <zhafifwistara1@gmail.com>
2026-01-13 13:45:17 +0700

    fix header

--- lib/layout.shared.tsx
+++ lib/layout.shared.tsx
@@ -13,7 +13,7 @@ export function baseOptions(): BaseLayoutProps {
             height={30}
             className="rounded-sm"
           />
-          <span className="font-bold text-lg tracking-tight">eSchool Website</span>
+          <span className="font-bold text-lg tracking-tight">eSchool Mobile - Siswa & Wali</span>
         </div>
       ),
     },
